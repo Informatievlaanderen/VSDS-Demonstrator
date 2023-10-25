7e1bf98ee1a68703d7e7ad315a0fdc28194d4b2f
jobulcke <127748878+jobulcke@users.noreply.github.com>
2023-10-25 16:53:39 +0200

    fix: indexes added + geo function removed (#42)

--- backend/src/main/java/be/informatievlaanderen/vsds/demonstrator/member/infra/MemberEntityJpaRepository.java
+++ backend/src/main/java/be/informatievlaanderen/vsds/demonstrator/member/infra/MemberEntityJpaRepository.java
@@ -1,6 +1,5 @@
 package be.informatievlaanderen.vsds.demonstrator.member.infra;
 
-import org.locationtech.jts.geom.Geometry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +9,8 @@ import java.util.List;
 
 public interface MemberEntityJpaRepository extends JpaRepository<MemberEntity, String> {
 
-    @Query(value = "select l from member_entity l where intersects(l.geometry, :geometry) = true and l.timestamp >= :startTime and l.timestamp <= :endTime and l.collection = :collectionName")
-    List<MemberEntity> getMemberGeometryEntitiesCoveredByGeometryInTimePeriodAndCollection(@Param("geometry") Geometry geometry, @Param("collectionName") String collectionName, @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
+    @Query(value = "select l from member_entity l where l.timestamp >= :startTime and l.timestamp <= :endTime and l.collection = :collectionName")
+    List<MemberEntity> getMemberGeometryEntitiesCoveredByGeometryInTimePeriodAndCollection(@Param("collectionName") String collectionName, @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
 
     List<MemberEntity> findByCollectionAndTimestampAfter(String collection, LocalDateTime localDateTime);
 
